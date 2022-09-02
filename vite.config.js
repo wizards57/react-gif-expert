5e1c51624b86a7be72e7ed7ae2c187f0d84cc08b
Fernando <wizards57@hotmail.com>
2022-09-01 23:01:50 -0500

    se modifico el vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+
+  //Prueba es cuando se sube al github pages fucione sin problemas eliminar si algo falla
+  base: "./",
+  build: {
+    outDir: "./docs",
+  },
+});
